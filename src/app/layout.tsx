b0fd4939d30d77b956c35744bfe62c221f81adf5
saivivek45 <154612247+saivivek45@users.noreply.github.com>
2025-06-21 13:37:24 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,14 @@ import "../styles/globals.css";
 import { Outfit } from 'next/font/google'
 
 const outfit = Outfit({
-  subsets:['latin'],
-  weight:['400','600','800'],
-})
+  subsets: ['latin'],
+  weight: ['400', '600', '800'],
+});
 
-export const metadata ={
-  title:"GiRi",
-  description:"Your Companion"
-}
+export const metadata = {
+  title: "GiRi",
+  description: "Your Companion"
+};
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -18,7 +18,7 @@ import Footer from "../components/Footer";
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-white text-black min-h-screen flex flex-col">
+      <body className={`${outfit.className} bg-white text-black min-h-screen flex flex-col`}>
         <Navbar />
         <main className="flex-1">{children}</main>
         <Footer />
